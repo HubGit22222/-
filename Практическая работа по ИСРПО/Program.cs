c434647116e63545de8e0c45cb6f2789a3849641
Anna <annahudorozko@gmail.com>
2024-04-12 20:41:02 +0700

    opyat poprosili cho-to dobavit(

--- Практическая работа по ИСРПО/Program.cs
+++ Практическая работа по ИСРПО/Program.cs
@@ -7,6 +7,7 @@
             Console.WriteLine("Это что-то нужное для практической работы");
             Console.WriteLine("Привет, мир");
             Console.WriteLine("Пока, мир");
+            Console.WriteLine("нужно было добавить изменение, вот оно)))");
         }
     }
 }
