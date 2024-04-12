9cfa11f9f22ca6fefe6271785ced10964fc50483
Anna <annahudorozko@gmail.com>
2024-04-12 20:24:09 +0700

    Добавьте файлы проекта.

---
+++ Практическая работа по ИСРПО/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Практическая_работа_по_ИСРПО
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Это что-то нужное для практической работы");
+            Console.WriteLine("Привет, мир");
+            Console.WriteLine("Пока, мир");
+        }
+    }
+}
